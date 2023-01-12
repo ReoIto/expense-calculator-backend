9bc4490549cd63091d19dd394d73342c8178dbb5
Reo Ito <leo.0210.ito@gmail.com>
2023-01-12 21:39:37 +0900

    corsエラー

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.hosts << ENV["FRONT_END_PROD_URL"]
 end
