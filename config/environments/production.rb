48045eddf9826cc7720733e31cd0678ba87e3893
Reo Ito <leo.0210.ito@gmail.com>
2023-01-12 22:07:38 +0900

    戻す

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.hosts << ENV["BACK_END_PROD_URL"]
 end
