f863068d0bf1ca6a164330e37d5d7971d68fc5be
Reo Ito <leo.0210.ito@gmail.com>
2023-01-12 21:28:44 +0900

    cors.rbに使用する環境変数名を修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", ENV["FRONT_END_PROD_DOMAIN"]
+    origins "localhost:8080", ENV["FRONT_END_PROD_URL"]
 
     resource "*",
       headers: :any,
